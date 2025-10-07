e375654e7eb068c831f1e062b75d081096a6226c
Thaddeus Kendrick Andrian <133015687+thzsszs04@users.noreply.github.com>
2025-10-07 23:59:39 +0700

    Add files via upload

---
+++ agent_data.js
@@ -0,0 +1,19 @@
+const agents = [
+    { name: "Brimstone", photo: "images/agents/image 1.png", role: "controller" },
+    { name: "Phoenix", photo: "images/agents/image 2.png", role: "duelist" },
+    { name: "Sage", photo: "images/agents/image 3.png", role: "sentinel" },
+    { name: "Sova", photo: "images/agents/image 4.png", role: "initiator" },
+    { name: "Viper", photo: "images/agents/image 5.png", role: "controller" },
+    { name: "Cypher", photo: "images/agents/image 6.png", role: "sentinel" },
+    { name: "Reyna", photo: "images/agents/image 7.png", role: "sentinel" },
+    { name: "Killjoy", photo: "images/agents/image 8.png", role: "initiator" },
+    { name: "Breach", photo: "images/agents/image 9.png", role: "initiator" },
+    { name: "Omen", photo: "images/agents/image 10.png", role: "controller" },
+    { name: "Yoru", photo: "images/agents/image 11.png", role: "duelist" },
+    { name: "Harbor", photo: "images/agents/image 12.png", role: "controller" },
+    { name: "Kay/O", photo: "images/agents/image 13.png", role: "initiator" },
+    { name: "Neon", photo: "images/agents/image 14.png", role: "duelist" },
+    { name: "Chamber", photo: "images/agents/image 15.png", role: "sentinel" },
+    { name: "Iso", photo: "images/agents/image 16.png", role: "duelist" },
+  ];
+  
